ec7a67b47dbea8992ce68ec8fc70232065791864
soheyl <soheylrashidsahebjam@gmail.com>
2025-04-22 04:04:36 -0700

    hello

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,7 +9,6 @@ return {
     config = function()
         vim.cmd("colorscheme boo")
         -- transparent background
-        --
         vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
         vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
         vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
