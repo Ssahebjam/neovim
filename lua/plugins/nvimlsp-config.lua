a379ee920c8012e0e71adc33dbffb1ec7c40d091
soheyl <soheylrashidsahebjam@gmail.com>
2025-04-29 23:49:33 -0700

    hello

--- lua/plugins/nvimlsp-config.lua
+++ lua/plugins/nvimlsp-config.lua
@@ -4,8 +4,7 @@ return {
     config = function()
         local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({ capabilities = capabilities })
+        local lspconfig = require("lspconfig") lspconfig.lua_ls.setup({ capabilities = capabilities })
         lspconfig.ts_ls.setup({ capabilities = capabilities })
         lspconfig.emmet_language_server.setup({ capabilities = capabilities })
         lspconfig.html.setup({ capabilities = capabilities })
@@ -16,5 +15,12 @@ return {
         vim.keymap.set("n", "K", vim.lsp.buf.hover )
         vim.keymap.set("n", "gd", vim.lsp.buf.definition )
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action )
+
+        vim.api.nvim_create_autocmd("CursorHold", {
+            callback = function()
+                vim.diagnostic.open_float(nil, { focus = false })
+            end
+        })
+
     end
 }
